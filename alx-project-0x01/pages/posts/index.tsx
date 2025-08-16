7ec72e70b88b427f8285eccf75118163f833fa25
Feyisayo <feyisayo233@gmail.com>
2025-08-16 15:14:48 +0100

    updated

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/layout/Header";
 import React from "react";
 
 const Posts: React.FC = () => (
-  <div className="p-8">
-    <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
-    {/* Add PostCard components here */}
+  <div className="flex flex-col min-h-screen">
+    <Header />
+    <main className="flex-grow p-8">
+      <h1 className="text-3xl font-bold mb-4">Posts Page</h1>
+      {/* Add PostCard components here */}
+    </main>
   </div>
 );
 
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/layout/Header";
 import React from "react";
 
 const Users: React.FC = () => (
-  <div className="p-8">
-    <h1 className="text-3xl font-bold mb-4">Users Page</h1>
-    {/* Add user components here */}
+  <div className="flex flex-col min-h-screen">
+    <Header />
+    <main className="flex-grow p-8">
+      <h1 className="text-3xl font-bold mb-4">Users Page</h1>
+      {/* Add user components here */}
+    </main>
   </div>
 );
 
